a43939e8582dbd0a9e4e35d6dccfdec6834c608d
Quietsy <quietsy@gmail.com>
2020-10-01 18:38:16 +0300

    Initial commit

---
+++ DiscordBot/Modules/PublicModule.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using DiscordBot.Services;
+
+namespace DiscordBot.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        public DiscordService service { get; set; }
+
+        [Command("ping")]
+        [Alias("pong", "hello")]
+        public Task PingAsync()
+            => ReplyAsync("pong!");
+
+        [Command("add")]
+        [Alias("a", "recommend")]
+        public async Task AddAsync([Remainder] string text)
+        {
+            var link = Context.Message.GetJumpUrl().Replace("https", "discord");
+            var user = Context.User.ToString();
+            var channel = Context.Channel.ToString();
+            await service.AddDiscordEntryAsync(channel, user, text, link);
+        }
+/*
+        [Command("userinfo")]
+        public async Task UserInfoAsync(IUser user = null)
+        {
+            user = user ?? Context.User;
+
+            await ReplyAsync(user.ToString());
+        }
+
+        [Command("ban")]
+        [RequireContext(ContextType.Guild)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        public async Task BanUserAsync(IGuildUser user, [Remainder] string reason = null)
+        {
+            await user.Guild.AddBanAsync(user, reason: reason);
+            await ReplyAsync("ok!");
+        }
+
+        [Command("echo")]
+        public Task EchoAsync([Remainder] string text)
+            => ReplyAsync('\u200B' + text);
+
+        [Command("list")]
+        public Task ListAsync(params string[] objects)
+            => ReplyAsync("You listed: " + string.Join("; ", objects));
+
+        [Command("guild_only")]
+        [RequireContext(ContextType.Guild, ErrorMessage = "Sorry, this command must be ran from within a server, not a DM!")]
+        public Task GuildOnlyCommand()
+            => ReplyAsync("Nothing to see here!");*/
+    }
+}
---
+++ DiscordBot/Services/DiscordService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DiscordBot.Services
+{
+    public class DiscordService
+    {
+        private readonly HttpClient _http;
+
+        public DiscordService(HttpClient http)
+            => _http = http;
+
+        public async Task AddDiscordEntryAsync(string channel, string user, string text, string link)
+        {
+            string json = JsonConvert.SerializeObject(new
+            {
+                channelName = channel,
+                username = user,
+                message = text,
+                messageLink = link
+            });
+
+            var discordweb = Environment.GetEnvironmentVariable("discordweb");
+            var content = new StringContent(json.ToString(), Encoding.UTF8, "application/json");
+            await _http.PostAsync(discordweb, content);
+        }
+    }
+}
